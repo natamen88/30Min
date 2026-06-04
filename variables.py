17b1992c731b5077ba30f82b095b9e21a34ae0fe
Nataniel Mendoza <natamen@yahoo.com>
2026-06-04 15:57:44 -0600

    mas variables

--- variables.py
+++ variables.py
@@ -1,2 +1,8 @@
 name = 'Bob'
-print ('Hello ' + name)
+number = 10
+decimal = 10.123
+has_money = True
+names = ['Natalia', 'Hanna', 'Emma', 'Nataniel']
+users = {'Natalia': 1, 'Hanna': 2, 'Emma': 3, 'Nataniel': 4}
+print (users['Natalia'])
+
